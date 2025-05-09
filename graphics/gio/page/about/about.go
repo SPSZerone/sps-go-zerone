e08380d37e999f8218b6f6b0d0b5d7db90c9c0d5
SPSZerone <12197072+SPSZerone@users.noreply.github.com>
2025-05-09 08:17:41 +0800

    gio

--- graphics/gio/page/about/about.go
+++ graphics/gio/page/about/about.go
@@ -3,12 +3,13 @@ package about
 import (
 	"gioui.org/io/event"
 	"gioui.org/layout"
+	"gioui.org/unit"
 	"gioui.org/widget/material"
 	"gioui.org/x/component"
 
 	spsgio "github.com/SPSZerone/sps-go-zerone/graphics/gio"
 	spsicon "github.com/SPSZerone/sps-go-zerone/graphics/gio/icon"
-	spslayout "github.com/SPSZerone/sps-go-zerone/graphics/gio/layout"
+	spscopy "github.com/SPSZerone/sps-go-zerone/graphics/gio/widget/copy"
 	spslist "github.com/SPSZerone/sps-go-zerone/graphics/gio/widget/list"
 )
 
@@ -30,6 +31,11 @@ var _ spsgio.Page = (*Page)(nil)
 type Page struct {
 	spslist.List
 	spsgio.Pages
+
+	CopyName     spscopy.Copy
+	CopyAuthor   spscopy.Copy
+	CopyLicense  spscopy.Copy
+	CopyHomePage spscopy.Copy
 }
 
 func (p *Page) Actions() []component.AppBarAction {
@@ -64,35 +70,44 @@ func (p *Page) Layout(win spsgio.Window, gtx layout.Context, param any) layout.D
 			Axis:      layout.Vertical,
 		}.Layout(gtx,
 			layout.Rigid(func(gtx layout.Context) layout.Dimensions {
-				return p.LayoutInfo(theme, gtx, "Name", Name)
+				return p.LayoutInfo(theme, gtx, "Name", Name, &p.CopyName)
 			}),
 			layout.Rigid(func(gtx layout.Context) layout.Dimensions {
-				return p.LayoutInfo(theme, gtx, "Author", Author)
+				return p.LayoutInfo(theme, gtx, "Author", Author, &p.CopyAuthor)
 			}),
 			layout.Rigid(func(gtx layout.Context) layout.Dimensions {
-				return p.LayoutInfo(theme, gtx, "License", License)
+				return p.LayoutInfo(theme, gtx, "License", License, &p.CopyLicense)
 			}),
 			layout.Rigid(func(gtx layout.Context) layout.Dimensions {
-				return p.LayoutInfo(theme, gtx, "HomePage", HomePage)
+				return p.LayoutInfo(theme, gtx, "HomePage", HomePage, &p.CopyHomePage)
 			}),
 			//children...,
 		)
 	})
 }
 
-func (p *Page) LayoutInfo(theme *material.Theme, gtx layout.Context, name, value string) layout.Dimensions {
-	return spslayout.FlexInset{}.LayoutFlexedWidgetAB(
+func (p *Page) LayoutInfo(theme *material.Theme, gtx layout.Context, name, value string, copy *spscopy.Copy) layout.Dimensions {
+	return copy.LayoutCopyFlexChild(
+		theme,
 		gtx,
-		0.2,
-		func(gtx layout.Context) layout.Dimensions {
-			labelStyle := material.H6(theme, name)
-			//labelStyle.Font.Typeface = spsnerdfont.MesloLGSNerdFontMono
-			return labelStyle.Layout(gtx)
+		func() string {
+			return value
 		},
-		func(gtx layout.Context) layout.Dimensions {
-			labelStyle := material.Body1(theme, value)
-			//labelStyle.Font.Typeface = spsnerdfont.MesloLGSNerdFontMono
-			return labelStyle.Layout(gtx)
-		},
+		layout.Flexed(0.2, func(gtx layout.Context) layout.Dimensions {
+			return layout.Inset{
+				Left:   unit.Dp(8),
+				Right:  unit.Dp(8),
+				Top:    unit.Dp(4),
+				Bottom: unit.Dp(4),
+			}.Layout(gtx, material.H6(theme, name).Layout)
+		}),
+		layout.Flexed(0.8, func(gtx layout.Context) layout.Dimensions {
+			return layout.Inset{
+				Left:   unit.Dp(8),
+				Right:  unit.Dp(8),
+				Top:    unit.Dp(4),
+				Bottom: unit.Dp(4),
+			}.Layout(gtx, material.Body1(theme, value).Layout)
+		}),
 	)
 }

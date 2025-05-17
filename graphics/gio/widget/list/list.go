213295a310c5164f04ff1b71b5ca118485af2946
SPSZerone <12197072+SPSZerone@users.noreply.github.com>
2025-05-17 09:28:09 +0800

    gio

--- graphics/gio/widget/list/list.go
+++ graphics/gio/widget/list/list.go
@@ -16,13 +16,38 @@ func New() List {
 	}
 }
 
+type ScrollBarMode int
+
+const (
+	ScrollBarModeAuto ScrollBarMode = iota
+	ScrollBarModeEnable
+	ScrollBarModeDisable
+)
+
 type List struct {
 	widget.List
+
+	ScrollBarMode ScrollBarMode
 }
 
 func (p *List) Layout(
 	theme *material.Theme, gtx layout.Context,
 	length int, element layout.ListElement,
 ) layout.Dimensions {
-	return material.List(theme, &p.List).Layout(gtx, length, element)
+	var withScrollbar bool
+
+	switch p.ScrollBarMode {
+	case ScrollBarModeAuto:
+		if p.List.Axis == layout.Horizontal {
+			withScrollbar = true
+		}
+	case ScrollBarModeEnable:
+		withScrollbar = true
+	case ScrollBarModeDisable:
+	}
+
+	if withScrollbar {
+		return material.List(theme, &p.List).Layout(gtx, length, element)
+	}
+	return p.List.Layout(gtx, length, element)
 }

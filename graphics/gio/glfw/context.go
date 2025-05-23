e267d462f06b7654e17d6a1d0bd4c52874224968
SPSZerone <12197072+SPSZerone@users.noreply.github.com>
2025-05-23 09:13:38 +0800

    gio

--- graphics/gio/glfw/context.go
+++ graphics/gio/glfw/context.go
@@ -1,11 +1,18 @@
 package glfw
 
 import (
+	"image"
+	"time"
+
 	"gioui.org/gpu"
 	"gioui.org/io/input"
+	"gioui.org/layout"
 	"gioui.org/op"
+	"gioui.org/unit"
 	"gioui.org/widget/material"
 	"github.com/rs/zerolog"
+
+	"github.com/go-gl/glfw/v3.3/glfw"
 
 	spsgio "github.com/SPSZerone/sps-go-zerone/graphics/gio"
 	spslog "github.com/SPSZerone/sps-go-zerone/log/zerolog"
@@ -35,11 +42,34 @@ func NewContext() Context {
 type Context struct {
 	GPU gpu.GPU
 
-	Theme *material.Theme
-	Ops   op.Ops
-	Queue input.Router
+	Theme  *material.Theme
+	Ops    op.Ops
+	Router input.Router
 
 	Logger zerolog.Logger
+}
+
+func (c *Context) LayoutContext(window *glfw.Window) (gtx layout.Context, size image.Point) {
+	scale, _ := window.GetContentScale()
+	width, height := window.GetFramebufferSize()
+	size = image.Point{X: width, Y: height}
+	c.Ops.Reset()
+	gtx = layout.Context{
+		Ops:    &c.Ops,
+		Now:    time.Now(),
+		Source: c.Router.Source(),
+		Metric: unit.Metric{
+			PxPerDp: scale,
+			PxPerSp: scale,
+		},
+		Constraints: layout.Exact(size),
+	}
+	return
+}
+
+func (c *Context) Frame(gtx layout.Context, size image.Point) {
+	c.GPU.Frame(gtx.Ops, gpu.OpenGLRenderTarget{}, size)
+	c.Router.Frame(gtx.Ops)
 }
 
 func (c *Context) Release() {

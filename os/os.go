315e5591bd36b95557b53b765281a88f42552262
SPSZerone <12197072+SPSZerone@users.noreply.github.com>
2025-07-06 10:28:26 +0800

    os

--- os/os.go
+++ os/os.go
@@ -7,3 +7,15 @@ import (
 func IsDarwin() bool {
 	return runtime.GOOS == "darwin"
 }
+
+func IsFreeBSD() bool {
+	return runtime.GOOS == "freebsd"
+}
+
+func IsLinux() bool {
+	return runtime.GOOS == "linux"
+}
+
+func IsWindows() bool {
+	return runtime.GOOS == "windows"
+}

5ed85c23154611a43b1ecf1b469dd35f4aad9e16
SPSZerone <12197072+SPSZerone@users.noreply.github.com>
2026-01-09 20:02:42 +0800

    zerolog

--- log/zerolog/sgr.go
+++ log/zerolog/sgr.go
@@ -8,7 +8,7 @@ import (
 
 func SGRTimestamp() ansi.SGR {
 	var sgr ansi.SGR
-	opts := []ansi.SGROption{ansi.SGRoBold(), ansi.SGRoColor(ansi.SGROptFGColorBlack), ansi.SGRoColor(ansi.SGROptBrightBGColorBlue)}
+	opts := []ansi.SGROption{ansi.SGRoBold(), ansi.SGRoColor(ansi.SGROptFGColorBlack), ansi.SGRoColor(ansi.SGROptBrightBGColorCyan)}
 	sgr.Options.Opts(opts...)
 	return sgr
 }
@@ -20,7 +20,7 @@ func SGRLevel(level zerolog.Level) ansi.SGR {
 
 	switch level {
 	case zerolog.DebugLevel:
-		sgr.Options.Opts(ansi.SGRoColor(ansi.SGROptBrightBGColorBlue))
+		sgr.Options.Opts(ansi.SGRoColor(ansi.SGROptBrightBGColorCyan))
 	case zerolog.InfoLevel:
 		sgr.Options.Opts(ansi.SGRoColor(ansi.SGROptBrightBGColorGreen))
 	case zerolog.WarnLevel:
@@ -37,56 +37,56 @@ func SGRLevel(level zerolog.Level) ansi.SGR {
 
 func SGRCaller() ansi.SGR {
 	var sgr ansi.SGR
-	opts := []ansi.SGROption{ansi.SGRoBold(), ansi.SGRoUnderline(), ansi.SGRoFGColorCustomIndex(165), ansi.SGRoColor(ansi.SGROptBGColorBlack)}
+	opts := []ansi.SGROption{ansi.SGRoBold(), ansi.SGRoUnderline(), ansi.SGRoFGColorCustomIndex(165), ansi.SGRoColor(ansi.SGROptBrightBGColorWhite)}
 	sgr.Options.Opts(opts...)
 	return sgr
 }
 
 func SGRMessage() ansi.SGR {
 	var sgr ansi.SGR
-	opts := []ansi.SGROption{ansi.SGRoColor(ansi.SGROptBrightFGColorWhite), ansi.SGRoColor(ansi.SGROptBGColorBlack)}
+	opts := []ansi.SGROption{ansi.SGRoBold(), ansi.SGRoColor(ansi.SGROptFGColorBlack), ansi.SGRoColor(ansi.SGROptBrightBGColorWhite)}
 	sgr.Options.Opts(opts...)
 	return sgr
 }
 
 func SGRFieldName() ansi.SGR {
 	var sgr ansi.SGR
-	opts := []ansi.SGROption{ansi.SGRoBold(), ansi.SGRoItalic(), ansi.SGRoColor(ansi.SGROptFGColorBlack), ansi.SGRoColor(ansi.SGROptBrightBGColorGreen)}
+	opts := []ansi.SGROption{ansi.SGRoBold(), ansi.SGRoItalic(), ansi.SGRoFGColorCustomRGB(0xFF, 0xFF, 0xFF), ansi.SGRoColor(ansi.SGROptBrightBGColorGreen)}
 	sgr.Options.Opts(opts...)
 	return sgr
 }
 
 func SGRFieldSeparator() ansi.SGR {
 	var sgr ansi.SGR
-	opts := []ansi.SGROption{ansi.SGRoColor(ansi.SGROptBrightFGColorWhite), ansi.SGRoColor(ansi.SGROptBrightBGColorGreen)}
+	opts := []ansi.SGROption{ansi.SGRoBold(), ansi.SGRoFGColorCustomRGB(0x0, 0x0, 0x0), ansi.SGRoColor(ansi.SGROptBrightBGColorGreen)}
 	sgr.Options.Opts(opts...)
 	return sgr
 }
 
 func SGRFieldValue() ansi.SGR {
 	var sgr ansi.SGR
-	opts := []ansi.SGROption{ansi.SGRoBold(), ansi.SGRoItalic(), ansi.SGRoColor(ansi.SGROptFGColorBlack), ansi.SGRoColor(ansi.SGROptBrightBGColorGreen)}
+	opts := []ansi.SGROption{ansi.SGRoBold(), ansi.SGRoItalic(), ansi.SGRoFGColorCustomRGB(0xFF, 0xFF, 0xFF), ansi.SGRoColor(ansi.SGROptBrightBGColorGreen)}
 	sgr.Options.Opts(opts...)
 	return sgr
 }
 
 func SGRErrFieldName() ansi.SGR {
 	var sgr ansi.SGR
-	opts := []ansi.SGROption{ansi.SGRoBold(), ansi.SGRoItalic(), ansi.SGRoColor(ansi.SGROptFGColorBlack), ansi.SGRoColor(ansi.SGROptBrightBGColorRed)}
+	opts := []ansi.SGROption{ansi.SGRoBold(), ansi.SGRoItalic(), ansi.SGRoFGColorCustomRGB(0xFF, 0xFF, 0xFF), ansi.SGRoColor(ansi.SGROptBrightBGColorRed)}
 	sgr.Options.Opts(opts...)
 	return sgr
 }
 
 func SGRErrFieldSeparator() ansi.SGR {
 	var sgr ansi.SGR
-	opts := []ansi.SGROption{ansi.SGRoColor(ansi.SGROptBrightFGColorWhite), ansi.SGRoColor(ansi.SGROptBrightBGColorRed)}
+	opts := []ansi.SGROption{ansi.SGRoBold(), ansi.SGRoFGColorCustomRGB(0x0, 0x0, 0x0), ansi.SGRoColor(ansi.SGROptBrightBGColorRed)}
 	sgr.Options.Opts(opts...)
 	return sgr
 }
 
 func SGRErrFieldValue() ansi.SGR {
 	var sgr ansi.SGR
-	opts := []ansi.SGROption{ansi.SGRoBold(), ansi.SGRoItalic(), ansi.SGRoColor(ansi.SGROptFGColorBlack), ansi.SGRoColor(ansi.SGROptBrightBGColorRed)}
+	opts := []ansi.SGROption{ansi.SGRoBold(), ansi.SGRoItalic(), ansi.SGRoFGColorCustomRGB(0xFF, 0xFF, 0xFF), ansi.SGRoColor(ansi.SGROptBrightBGColorRed)}
 	sgr.Options.Opts(opts...)
 	return sgr
 }

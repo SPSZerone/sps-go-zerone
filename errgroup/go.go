43a459de57ba0eb370415ca653fbd084d8fe29db
SPSZerone <12197072+SPSZerone@users.noreply.github.com>
2026-02-07 21:35:15 +0800

    errgroup

---
+++ errgroup/go.go
@@ -0,0 +1,36 @@
+package errgroup
+
+import (
+	"fmt"
+	"runtime/debug"
+
+	"golang.org/x/sync/errgroup"
+
+	spslog "github.com/SPSZerone/sps-go-zerone/log/zerolog"
+)
+
+type (
+	OnRecover func(r any)
+)
+
+func SafeGo(g *errgroup.Group, fn func() error, onRecover OnRecover) {
+	g.Go(func() error {
+		defer func() {
+			if r := recover(); r != nil {
+				err, ok := r.(error)
+				if !ok {
+					err = fmt.Errorf("%v", r)
+				}
+
+				stack := string(debug.Stack())
+
+				spslog.Logger.Error().Err(err).Msgf("[Recovery] panic -> %v\nStack trace:\n%s\n", err, stack)
+
+				if onRecover != nil {
+					onRecover(r)
+				}
+			}
+		}()
+		return fn()
+	})
+}
